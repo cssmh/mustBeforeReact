3d2c10969ede769398209710419db00128f5d656
momen210 <touristmomen@gmail.com>
2023-12-16 23:04:58 +0600

    add new

---
+++ math-min-max-ceil-floor.js
@@ -0,0 +1,27 @@
+// 1. Math.min(): Returns the smallest of zero or more numbers.
+const minValue = Math.min(2, 5, 1, 8, 4);
+console.log(minValue); // Output: 1
+
+// 2. Math.max(): Returns the largest of zero or more numbers.
+const maxValue = Math.max(2, 5, 1, 8, 4);
+console.log(maxValue); // Output: 8
+
+// 3. Math.ceil(): Rounds a number up to the nearest integer.
+const roundedUp = Math.ceil(5.3);
+console.log(roundedUp); // Output: 6
+
+// 4. Math.floor(): Rounds a number down to the nearest integer.
+const roundedDown = Math.floor(5.9);
+console.log(roundedDown); // Output: 5
+
+// 5. Math.abs(): Returns the absolute value of a number (removes the sign).
+const absoluteValue = Math.abs(-10);
+console.log(absoluteValue); // Output: 10
+
+// 6. Math.round(): Rounds a number to the nearest integer.
+const roundedValue = Math.round(5.4);
+console.log(roundedValue); // Output: 5
+
+// 7. Math.random(): Returns a pseudo-random number between 0 and 1 (exclusive).
+const randomValue = Math.random();
+console.log(randomValue); // Output: a random value between 0 (inclusive) and 1 (exclusive)
