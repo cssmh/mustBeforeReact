6b985c10c4ce200a43ee8b863594071cff10c289
momen210 <touristmomen@gmail.com>
2023-12-18 22:15:36 +0600

    add new one

---
+++ mustKnowHere/numberToString.js
@@ -0,0 +1,7 @@
+const num = 50;
+const res = num + "";
+console.log(res);
+
+const number = "50";
+const result = +number;
+console.log(result);
--- mustKnowHere/ternary.js
+++ mustKnowHere/ternary.js
@@ -1,4 +1,4 @@
-const money = 991;
+const money = 99;
 
-let food = money >= 100 ? "eat biriani" : "cha kha tui";
+const food = money >= 100 ? "eat biriani" : "cha kha tui";
 console.log(food);
